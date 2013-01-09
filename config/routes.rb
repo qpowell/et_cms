9af884b88e519af8415a0bc7ebd19ba6a82b98e5
Scott Olsen <solsen300@gmail.com>
2013-01-09 10:59:00 -0600

    removing 404 on sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 EtCms::Engine.routes.draw do
   root to: "pages#home", :via => :get
   mount Ckeditor::Engine => '/ckeditor'
-  match 'users/sign_up' => redirect('/404.html')
 
   devise_for :users, {
       class_name: 'EtCms::User',
