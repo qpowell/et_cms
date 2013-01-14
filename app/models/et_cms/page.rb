672545a45285196f0654b37b12681150364d5460
Scott Olsen <solsen300@gmail.com>
2013-01-14 12:50:03 -0600

    adding helpers to the page model

--- app/models/et_cms/page.rb
+++ app/models/et_cms/page.rb
@@ -1,8 +1,28 @@
 module EtCms
   class Page < ActiveRecord::Base
-    attr_accessible :browser_title, :content, :exclude_from_menu, :link_url, :list_order, :meta_description, :name, :parent_id, :status
+    attr_accessible :browser_title, :content, :exclude_from_menu, :link_url, :list_order, :meta_description, :name, :parent_id, :active
 
     extend FriendlyId
     friendly_id :name, use: :slugged
+
+    def has_children
+      Page.where(:parent_id => self.id, :active => true).count > 0
+    end
+
+    def children
+      Page.where(:parent_id => self.id, :active => true).order("list_order ASC")
+    end
+
+    def breadcrumbs
+      breadcrumb = %Q(<a href="#{self.slug}">#{self.name}</a>)
+      parent_id = self.parent_id
+      while (!parent_id.nil?)
+        puts parent_id
+        parent = Page.find(parent_id)
+        breadcrumb = %Q(<a href="#{parent.slug}">#{parent.name}</a>) + " > " + breadcrumb
+        parent_id = parent.parent_id
+      end
+      breadcrumb
+    end
   end
 end
